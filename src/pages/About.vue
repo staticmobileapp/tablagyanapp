628d3a2abd1f708e91d707f21a1e49f211b8dedc
Chetan Sachdev <cksachdev@gmail.com>
2019-09-10 07:12:58 +0530

    Added routes, updated icons and links. Added youtube links in sequence desired.
    Added location in About

---
+++ src/pages/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page padding>
+
+<p>Tabla Learning @ TablaGyan is not to be mistaken as only learning 'art of playing Tabla'. Tabla being basically an accompaniment instrument demands a basic if not an extensive knowledge of 'music' as well...!</p>
+<p>This being the case, learning at TablaGyan incorporates an interesting blend of learning music as whole with a special attention to Tabla..! </p>
+<p></p><h2>TablaGyan-the edge..!</h2><p></p>
+<p>TablaGyan much follows the quote - "PLAY THE MUSIC, NOT THE INSTRUMENT"</p>
+<p>Students learning at TablaGyan are sure to have an edge over others due to the unique methodology the school follows in rendering classes to the students. </p>
+<p></p><h2>TablaGyan - Get the facts right..</h2><p></p>
+<ul><li>Weekend sessions - maximum 6 sessions per month.</li>
+<li>Group sessions in batches each of around 1 hour duration.</li>
+<li>Each group contains 4-5 students of similar age-group/expertise.</li>
+<li>Instruments are provided at the class. Assistance provided to purchase.</li>
+<li>Regular monitoring of students progress in terms of practice / exercises / homework.</li>
+<li>Periodical competitions / tests conducted to gauge students performance.</li>
+<li>Continual and value added learning due to group discussions / demos / group activities held.</li>
+<li>e-learning encouraged - Facebook, Watsapp, Youtube  group accounts are enabled for student.</li></ul>
+<p></p><h2>TablaGyan - Performance &amp; Evaluation</h2><p></p>
+<p>The school maintains strict academics and individuals are monitored very closely. Backed up by the one-of-its-kind academic software, monthly performance of the student is published to the parents and a sophisticated grading system is followed. Periodical mock tests / competitions are held to keep the student abreast with the lessons taught. The deserving students are rewarded handsomely by the school and encouraged throughout..!</p>
+<p>The school is affiliated to some of the leading music universities such as Akhila Bharatiya Gandharva Maha Mandal, Miraj and Prayag Sangeet Samiti, Allahabad, which issues Degree, Diplomas in Music. All students are encouraged to take up these exams after completing 2 years of learning at the school.</p>
+<p></p><h2>TablaGyan - Vision &amp; Mission..</h2><p></p>
+<p>TablaGyan aims to provide Quality Knowledge to aspirants of music and nurture them into good music artists.</p>
+<p>As a matter of fact, TablaGyan strives hard to not only make the student play the instrument but to develop an attitude similar to that of a Music Artist (Rasika)..!</p>
+
+<ul>
+  <li><div class="thumb"> <a href="http://tablagyan.com/images/images/Tabla Classes 1.jpg"><img src="http://tablagyan.com/images/images/thumbs/Tabla Classes 1.jpg"></a></div> </li>
+  <li><div class="thumb"> <a href="http://tablagyan.com/images/images/Tabla Classes 2.jpg"><img src="http://tablagyan.com/images/images/thumbs/Tabla Classes 2.jpg"></a></div> </li>
+  <li><div class="thumb"> <a href="http://tablagyan.com/images/images/Tabla Classes 3.jpg"><img src="http://tablagyan.com/images/images/thumbs/Tabla Classes 3.jpg"></a></div></li>
+  <li><div class="thumb"> <a href="http://tablagyan.com/images/images/Tabla Classes 4.jpg"><img src="http://tablagyan.com/images/images/thumbs/Tabla Classes 4.jpg"></a></div></li>
+  <li><div class="thumb"><a href="http://tablagyan.com/images/images/Tabla Classes 5.jpg"><img src="http://tablagyan.com/images/images/thumbs/Tabla Classes 5.jpg"></a></div> </li>
+</ul>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
